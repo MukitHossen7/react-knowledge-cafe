4d41d62616cafd5aec9f8b15a8e90f04ab6fa111
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-22 00:49:51 +0600

    Times components add

--- src/components/Bookmarked/Bookmarked.jsx
+++ src/components/Bookmarked/Bookmarked.jsx
@@ -1,6 +1,9 @@
+import Times from "../Times/Times";
+
 const Bookmarked = () => {
   return (
     <div className="w-full md:w-4/12">
+      <Times></Times>
       <h1>Book Marked</h1>
     </div>
   );
---
+++ src/components/Times/Times.jsx
@@ -0,0 +1,13 @@
+const Times = () => {
+  return (
+    <div>
+      <div className="border border-[rgba(96,71,236,0.4)] rounded-xl bg-[rgba(96,71,236,0.1)]">
+        <h2 className="text-[#6047EC] font-bold text-2xl py-5 text-center">
+          Spent time on read : 177 min
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Times;

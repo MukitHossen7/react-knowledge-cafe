c1bc4526f76f832ddc65f52b70fcb312edd6ffb1
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-21 22:19:27 +0600

    fetch local api use in useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
 import Headers from "./components/Headers/Headers";
 
 function App() {
   return (
     <>
       <Headers></Headers>
+      <Blogs></Blogs>
     </>
   );
 }

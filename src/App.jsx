d14b7f3cdb69762e1b84b2c6cd89acc8ae20a00a
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-22 18:37:46 +0600

    set alert message in components

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
 import Bookmarked from "./components/Bookmarked/Bookmarked";
 import Headers from "./components/Headers/Headers";
+import { toast, ToastContainer } from "react-toastify";
 
 function App() {
   const [bookmarks, setBookmarks] = useState([]);
   const [times, setTimes] = useState(0);
+
   const handleBookmark = (markBlog) => {
     const newBookMark = [...bookmarks, markBlog];
     setBookmarks(newBookMark);
+    toast.success("Bookmark added Successful!", { autoClose: 1500 });
   };
   const handleTimer = (time, id) => {
     setTimes(times + time);
-    console.log(id);
+
     const remainingBookMark = bookmarks.filter(
       (bookmark) => bookmark.id !== id
     );
     setBookmarks(remainingBookMark);
+    toast.success("Time added Successful!", { autoClose: 1500 });
   };
   return (
     <div className="w-11/12 mx-auto">
@@ -31,6 +35,7 @@ function App() {
         {/* {bookmarks.map((bookmark) => (
           <Bookmarked key={bookmark.id} bookmark={bookmark}></Bookmarked>
         ))} */}
+        <ToastContainer />;
       </div>
     </div>
   );
